ac3ba250ae5a41978485fe91b1e01d8427065c1d
Travis Thieman <travis.thieman@gmail.com>
2015-05-11 14:10:13 -0400

    Add daemon entrypoint and preflight check

---
+++ dusty/daemon.py
@@ -0,0 +1,19 @@
+import sys
+import logging
+
+from pync import Notifier
+
+from .preflight import preflight_check
+from .notifier import notify
+
+def configure_logging():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.captureWarnings(True)
+
+def main():
+    notify('Dusty initializing...')
+    configure_logging()
+    preflight_check()
+
+if __name__ == '__main__':
+    main()
--- requirements.py
+++ requirements.py
@@ -1,6 +1,7 @@
 install_requires = [
     'docker-py>=1.2.0',
-    'watchdog>=0.8.0'
+    'watchdog>=0.8.0',
+    'pync>=1.6.0'
 ]
 
 test_requires = [
