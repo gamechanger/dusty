c90fd7d026cdeeff7d073c1d15ff550cc937f961
Travis Thieman <travis.thieman@gmail.com>
2015-05-11 16:06:31 -0400

    Set up a Unix socket we can use for input

--- dusty/daemon.py
+++ dusty/daemon.py
@@ -1,17 +1,49 @@
+import os
 import sys
 import logging
+import socket
 
 from .preflight import preflight_check
 from .notifier import notify
 
-def configure_logging():
+SOCKET_PATH = '/var/run/dusty/dusty.sock'
+
+def _configure_logging():
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     logging.captureWarnings(True)
 
+def _clean_up_existing_socket():
+    try:
+        os.unlink(SOCKET_PATH)
+    except OSError:
+        if os.path.exists(SOCKET_PATH):
+            raise
+
+def _listen_on_socket():
+    _clean_up_existing_socket()
+
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(SOCKET_PATH)
+    sock.listen(1)
+
+    notify('Dusty is listening for commands')
+
+    while True:
+        connection, client_address = sock.accept()
+        try:
+            while True:
+                data = connection.recv(1024)
+                if not data:
+                    break
+                print data
+        finally:
+            connection.close()
+
 def main():
     notify('Dusty initializing...')
-    configure_logging()
+    _configure_logging()
     preflight_check()
+    _listen_on_socket()
 
 if __name__ == '__main__':
     main()
