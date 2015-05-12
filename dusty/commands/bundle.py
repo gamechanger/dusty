8bb34f5c911c03370d853aea843e5e080344c14b
Travis Thieman <travis.thieman@gmail.com>
2015-05-12 16:31:22 -0400

    Read in app specs, plus output a fancy table for bundle list

--- dusty/commands/bundle.py
+++ dusty/commands/bundle.py
@@ -1,2 +1,15 @@
+# coding=utf-8
+
+from prettytable import PrettyTable
+
+from ..config import get_config_value
+from ..specs import get_specs
+
 def list_bundles():
-    return 'LISTIN MAH BUNDLES'
+    specs, activated_bundles = get_specs(), get_config_value('bundles')
+    table = PrettyTable(["Name", "Description", "Enabled?"])
+    for bundle, bundle_spec in specs['bundles'].iteritems():
+        table.add_row([bundle_spec['name'],
+                       bundle_spec['description'],
+                       u"✓" if bundle in activated_bundles else ""])
+    return table.get_string(sortby="Name")
---
+++ dusty/specs.py
@@ -0,0 +1,17 @@
+import os
+import glob
+import yaml
+
+from .config import get_config_value
+
+def get_specs():
+    specs_path = get_config_value('specs_path')
+    specs = {}
+    for key in ['bundles', 'apps', 'libs', 'services']:
+        specs[key] = {}
+        key_path = os.path.join(specs_path, key)
+        for spec_path in glob.glob('{}/*.yml'.format(key_path)):
+            spec_name = os.path.splitext(os.path.split(spec_path)[-1])[0]
+            with open(spec_path, 'r') as f:
+                specs[key][spec_name] = yaml.load(f.read())
+    return specs
