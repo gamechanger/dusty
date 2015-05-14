51cbd0df8f1b3e583646732f9360e541f92cd271
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-05-14 18:31:44 -0400

    cleanup stuff

--- dusty/compiler/__init__.py
+++ dusty/compiler/__init__.py
@@ -1,12 +1,6 @@
 import pprint
 
-
-from .process_bundle import get_expanded_active_specs
-from ..specs import get_specs
-from ..config import get_config_value
+from .spec_assembler import get_assembled_specs
 
 def list_processed_specs():
-    activated_bundles = set(get_config_value('bundles'))
-    specs = get_specs()
-    processed_specs = get_expanded_active_specs(activated_bundles, specs)
-    yield pprint.pformat(processed_specs)
+    yield pprint.pformat(get_assembled_specs())
