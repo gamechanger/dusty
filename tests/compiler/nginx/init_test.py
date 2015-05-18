713664e3d570e959ec366354484bb8169ab9491d
paetling <paetling@gmail.com>
2015-05-18 17:59:43 -0400

    nginx conf stuff

--- tests/compiler/nginx/init_test.py
+++ tests/compiler/nginx/init_test.py
@@ -42,6 +42,36 @@ class TestPortSpecCompiler(TestCase):
         output = clense(nginx_configuration_spec(self.port_spec_dict_2))
         self.assertEqual(output, expected_output)
 
+    def test_nginx_configuration_spec_3(self):
+        port_spec = {'nginx': [{'proxied_ip': '127.0.0.0',
+                                'proxied_port':'8000',
+                                'host_address': 'local.gcapi.com',
+                                'host_port': '8001'},
+                               {'proxied_ip': '127.0.0.1',
+                                'proxied_port':'80',
+                                'host_address': 'local.gc.com',
+                                'host_port': '80'}]}
+
+        expected_output = clense("""http {
+            server {
+                listen local.gcapi.com:8001;
+                location / {
+                    proxy_pass 127.0.0.0:8000;
+                }
+            }
+            server {
+                listen local.gc.com:80;
+                location / {
+                    proxy_pass 127.0.0.1:80;
+                }
+            }
+        }
+        """)
+        output = clense(nginx_configuration_spec(port_spec))
+        print output
+        self.assertEqual(output, expected_output)
+
+
     def test_nginx_listen_string_1(self):
         self.assertEqual("listen local.gc.com:80;", _nginx_listen_string(self.port_spec_dict_1['nginx'][0]))
 
