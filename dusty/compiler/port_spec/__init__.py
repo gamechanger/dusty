93b80d33652ef360495aff35a0f30842a9578d8b
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-05-18 16:28:20 -0400

    check the key instead of try-except

--- dusty/compiler/port_spec/__init__.py
+++ dusty/compiler/port_spec/__init__.py
@@ -37,10 +37,9 @@ def port_spec_document(expanded_active_specs):
     # port_spec with respect to the apps and the ports they are outputted on
     for app_name in sorted(expanded_active_specs['apps'].keys()):
         app_spec = expanded_active_specs['apps'][app_name]
-        try:
-            host_forwarding_spec = app_spec['host_forwarding']
-        except KeyError:
+        if 'host_forwarding' not in app_spec:
             continue
+        host_forwarding_spec = app_spec['host_forwarding']
         container_port = host_forwarding_spec['container_port']
         host_name = host_forwarding_spec['host_name']
         host_full_address = '{}:{}'.format(host_name, host_forwarding_spec['host_port'])
