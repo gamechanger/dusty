ad5c37c2958eac6c13e5241ac98dd3ff658b73ac
Travis Thieman <travis.thieman@gmail.com>
2015-05-18 10:33:07 -0400

    Wrap notifications in a try

--- dusty/notifier.py
+++ dusty/notifier.py
@@ -1,8 +1,14 @@
 import os
+import logging
 
 from pync import Notifier
 
 PROCESS_ID = os.getpid()
 
 def notify(message):
-    Notifier.notify(message, title='Dusty', group=PROCESS_ID)
+    # This will fail, among other things, if you try to call this function
+    # on a non-OS X system (like Linux when we're running tests)
+    try:
+        Notifier.notify(message, title='Dusty', group=PROCESS_ID)
+    except:
+        logging.exception('Exception when attempting to perform notification')
