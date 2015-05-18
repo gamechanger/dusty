11bbe8c83252d128ffa60682eb740d17234ea472
Travis Thieman <travis.thieman@gmail.com>
2015-05-18 10:38:27 -0400

    Idiot library developers

--- dusty/notifier.py
+++ dusty/notifier.py
@@ -1,14 +1,15 @@
 import os
 import logging
 
-from pync import Notifier
-
 PROCESS_ID = os.getpid()
 
 def notify(message):
     # This will fail, among other things, if you try to call this function
-    # on a non-OS X system (like Linux when we're running tests)
+    # on a non-OS X system (like Linux when we're running tests).
+    # Also, the stupid library throws the exception on import, so we're
+    # forced to do the import here.
     try:
+        from pync import Notifier
         Notifier.notify(message, title='Dusty', group=PROCESS_ID)
     except:
         logging.exception('Exception when attempting to perform notification')
