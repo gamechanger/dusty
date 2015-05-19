95d013ebc3ce74816a395b69f72e44c9fa1d0a9e
paetling <paetling@gmail.com>
2015-05-19 13:43:52 -0400

    add in safe guards to stop people from adding new keys and changing non string keys

--- dusty/commands/manage_config.py
+++ dusty/commands/manage_config.py
@@ -5,7 +5,12 @@ def list_config():
 
 def set_value(key, value):
     config = get_config()
-    config[key] = value
-    save_config(config)
-    yield 'set {} to {} in your config'.format(key, value)
+    if key not in config:
+        yield 'key {} is not in config. Keep your config clean'.format(key)
+    elif not isinstance(config[key], basestring):
+        yield 'You can only modify string values in your config. {} has type {}'.format(key, type(config[key]))
+    else:
+        config[key] = value
+        save_config(config)
+        yield 'set {} to {} in your config'.format(key, value)
 
