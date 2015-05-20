ec39598279b1720fa8afca08a884d80f416571fc
paetling <paetling@gmail.com>
2015-05-20 11:32:57 -0400

    no name conflict

--- dusty/commands/run.py
+++ dusty/commands/run.py
@@ -8,11 +8,11 @@ def start_local_env():
     systems will in turn launch the services needed to make the
     local environment go"""
     assembled_spec = spec_assembler.get_assembled_specs()
-    port_spec = port_spec.get_port_spec_document(assembled_spec)
-    nginx_config = nginx.get_nginx_configuration_spec(port_spec)
-    compose_config = compose.get_compose_dict(assembled_spec, port_spec)
+    port_specification = port_spec.get_port_spec_document(assembled_spec)
+    nginx_config = nginx.get_nginx_configuration_spec(port_specification)
+    compose_config = compose.get_compose_dict(assembled_spec, port_specification)
 
-    hosts.update_hosts_file_from_port_spec(port_spec)
-    virtualbox.update_virtualbox_port_forwarding_from_port_spec(port_spec)
+    hosts.update_hosts_file_from_port_spec(port_specification)
+    virtualbox.update_virtualbox_port_forwarding_from_port_spec(port_specification)
     nginx.update_nginx_from_config(nginx_config)
     compose.update_running_containers_from_spec(compose_config)
