79e6bf363fe65ee3a1cfad9f62e8de0a8137cce9
paetling <paetling@gmail.com>
2015-05-20 11:29:22 -0400

    add in the run command

--- dusty/commands/run.py
+++ dusty/commands/run.py
@@ -1,5 +1,6 @@
 
 from ..compiler import compose, nginx, port_spec, spec_assembler
+from ..systems import compose, hosts, nginx, virtualbox
 
 def start_local_env():
     """ This command will use the compilers to get compose specs
@@ -8,3 +9,10 @@ def start_local_env():
     local environment go"""
     assembled_spec = spec_assembler.get_assembled_specs()
     port_spec = port_spec.get_port_spec_document(assembled_spec)
+    nginx_config = nginx.get_nginx_configuration_spec(port_spec)
+    compose_config = compose.get_compose_dict(assembled_spec, port_spec)
+
+    hosts.update_hosts_file_from_port_spec(port_spec)
+    virtualbox.update_virtualbox_port_forwarding_from_port_spec(port_spec)
+    nginx.update_nginx_from_config(nginx_config)
+    compose.update_running_containers_from_spec(compose_config)
