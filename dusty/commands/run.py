acb38606f85905f5f60fef9cea70c624aa45e22b
Travis Thieman <travis.thieman@gmail.com>
2015-05-22 12:07:37 -0400

    We don't want to call compilers from inside systems

--- dusty/commands/run.py
+++ dusty/commands/run.py
@@ -1,13 +1,13 @@
-
 from ..compiler import (compose as compose_compiler, nginx as nginx_compiler,
                         port_spec as port_spec_compiler, spec_assembler)
-from ..systems import compose, hosts, nginx, virtualbox
+from ..systems import compose, hosts, nginx, virtualbox, rsync
 
 def start_local_env():
     """ This command will use the compilers to get compose specs
     will pass those specs to the systems that need them. Those
     systems will in turn launch the services needed to make the
     local environment go"""
+    active_repos = spec_assembler.get_all_repos(active_only=True, include_specs_repo=False)
     assembled_spec = spec_assembler.get_assembled_specs()
     port_spec = port_spec_compiler.get_port_spec_document(assembled_spec)
     nginx_config = nginx_compiler.get_nginx_configuration_spec(port_spec)
@@ -16,6 +16,7 @@ def start_local_env():
     hosts.update_hosts_file_from_port_spec(port_spec)
     virtualbox.initialize_docker_vm()
     virtualbox.update_virtualbox_port_forwarding_from_port_spec(port_spec)
+    rsync.sync_repos(active_repos)
     nginx.update_nginx_from_config(nginx_config)
     compose.update_running_containers_from_spec(compose_config)
 
--- dusty/systems/rsync/__init__.py
+++ dusty/systems/rsync/__init__.py
@@ -3,7 +3,6 @@ import logging
 from subprocess import check_call
 
 from ... import constants
-from ...compiler.spec_assembler import get_all_repos
 from ...config import get_config_value, assert_config_key
 from ...demote import check_call_demoted
 from ...source import repo_path, repo_is_overridden
@@ -19,10 +18,10 @@ def _sync_dir(local_dir, remote_dir):
     logging.debug('Executing rsync command: {}'.format(' '.join(command)))
     check_call(command)
 
-def sync_active_repos():
+def sync_repos(repos):
     logging.info('Syncing repos over rsync')
     assert_config_key('mac_username')
-    for repo_name in get_all_repos(active_only=True, include_specs_repo=False):
+    for repo_name in repos:
         repo_type = 'overridden' if repo_is_overridden(repo_name) else 'Dusty-managed'
         remote_path = os.path.join(constants.VM_REPOS_DIR, repo_name)
         logging.info('Syncing {} repo {} to remote at {}'.format(repo_type, repo_name, remote_path))
