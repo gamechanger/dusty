0a1b4f29c682061b92cdc4298954961df768fff4
paetling <paetling@gmail.com>
2015-05-26 17:56:34 -0400

    add in passing tests

--- tests/compiler/test_test_cases.py
+++ tests/compiler/test_test_cases.py
@@ -148,3 +148,7 @@ class TestSpecAssemblerGetRepoTestCases(TestCase):
 
     def test_get_repo_of_app_or_service_lib(self):
         self.assertEqual(spec_assembler.get_repo_of_app_or_service('lib-a'), 'github.com/lib/a')
+
+    def test_get_repo_of_app_or_service_neither(self):
+        with self.assertRaises(KeyError):
+            spec_assembler.get_repo_of_app_or_service('lib-b')
