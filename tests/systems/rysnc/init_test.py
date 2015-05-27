c1017b7c683df98b11ddbafad2110b811306e64f
paetling <paetling@gmail.com>
2015-05-27 18:16:35 -0400

    passing tests and shit

--- tests/systems/rysnc/init_test.py
+++ tests/systems/rysnc/init_test.py
@@ -10,7 +10,7 @@ class TestRysnc(TestCase):
     @patch('dusty.systems.rsync.get_assembled_specs')
     @patch('dusty.compiler.spec_assembler.get_specs')
     def test_sync_repos_by_app_name_1(self, fake_get_specs, fake_get_assembled_specs, fake_sync_repos):
-        fake_get_assembled_specs.return_value = {
+        specs = {
             'apps': {
                 'app-a': {
                     'repo': 'github.com/app/a',
@@ -20,6 +20,10 @@ class TestRysnc(TestCase):
                     }
                 },
                 'app-b': {
+                    'depends': {
+                        'apps': {},
+                        'libs': {}
+                    },
                     'repo': 'github.com/app/b'}
             },
             'libs': {
@@ -28,10 +32,12 @@ class TestRysnc(TestCase):
                     'depends': {'libs': ['lib-b']}
                 },
                 'lib-b':{
+                    'depends': {},
                     'repo': 'github.com/lib/b'}
             }
         }
-        fake_get_specs.return_value == fake_get_assembled_specs.return_value
+        fake_get_assembled_specs.return_value = specs
+        fake_get_specs.return_value = specs
         sync_repos_by_app_name(['app-a', 'app-b'])
         fake_sync_repos.assert_has_calls([call(set(['github.com/app/a', 'github.com/app/b', 'github.com/lib/a', 'github.com/lib/b']))])
 
@@ -40,7 +46,7 @@ class TestRysnc(TestCase):
     @patch('dusty.systems.rsync.get_assembled_specs')
     @patch('dusty.compiler.spec_assembler.get_specs')
     def test_sync_repos_by_app_name_2(self, fake_get_specs, fake_get_assembled_specs, fake_sync_repos):
-        fake_get_assembled_specs.return_value = {
+        specs = {
             'apps': {
                 'app-a': {
                     'repo': 'github.com/app/a',
@@ -58,6 +64,7 @@ class TestRysnc(TestCase):
                     'repo': 'github.com/lib/b'}
             }
         }
-        fake_get_specs.return_value == fake_get_assembled_specs.return_value
+        fake_get_assembled_specs.return_value = specs
+        fake_get_specs.return_value = specs
         sync_repos_by_app_name(['app-a'])
         fake_sync_repos.assert_has_calls([call(set(['github.com/app/a', 'github.com/lib/a', 'github.com/lib/b']))])
