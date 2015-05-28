6bdc845337dc82127e160ac0eff4262efdaf872f
Travis Thieman <travis.thieman@gmail.com>
2015-05-28 14:29:41 -0400

    Better util abstraction for execing docker

--- dusty/commands/logs.py
+++ dusty/commands/logs.py
@@ -1,8 +1,8 @@
 import os
 
 from ..systems.compose import get_dusty_containers
-from .utils import _executable_path
+from .utils import exec_docker
 
 def tail_container_logs(app_or_service_name):
     container = get_dusty_containers([app_or_service_name])[0]
-    os.execl(_executable_path('docker'), 'docker', 'logs', '-f', container['Id'])
+    exec_docker('logs', '-f', container['Id'])
--- dusty/commands/utils.py
+++ dusty/commands/utils.py
@@ -1,4 +1,8 @@
+import os
 import subprocess
 
 def _executable_path(executable_name):
     return subprocess.check_output(['which', executable_name]).strip()
+
+def exec_docker(*args):
+    os.execl(_executable_path('docker'), 'docker', *args)
