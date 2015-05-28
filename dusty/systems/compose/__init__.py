84211867f179a53d076032391d04b61bd43db884
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-05-28 14:50:37 -0400

    error handling

--- dusty/systems/compose/__init__.py
+++ dusty/systems/compose/__init__.py
@@ -154,10 +154,15 @@ def _get_exited_dusty_containers(client):
 def remove_exited_dusty_containers():
     client = _get_docker_client()
     exited_containers = _get_exited_dusty_containers(client)
+    removed_containers = []
     for container in exited_containers:
         log_to_client("Removing container {}".format(container['Names'][0]))
-        client.remove_container(container['Id'])
-    return exited_containers
+        try:
+            client.remove_container(container['Id'])
+            removed_containers.append(container)
+        except Exception as e:
+            log_to_client(e.message or str(e))
+    return removed_containers
 
 def remove_unreferenced_images():
     client = _get_docker_client()
