0c5fa572833c02cba241b11d0a44349d71da25e0
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-05-29 16:36:43 -0400

    integrated schemer a little

--- dusty/commands/validate.py
+++ dusty/commands/validate.py
@@ -1,5 +1,13 @@
+import logging
+
+from schemer import Schema
+
+from ..compiler.spec_assembler import get_specs_path, get_specs_from_path
+from ..schemas import app_schema
+
 def validate_specs_from_path(specs_path):
-    pass
+    logging.info("Validating specs at path {}".format(specs_path))
+    specs = get_specs_from_path(specs_path)
 
 def validate_specs():
-    pass
+    validate_specs_from_path(get_specs_path())
