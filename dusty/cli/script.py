0a03e0a4cea9eb62771141d5494a446a34508706
Travis Thieman <travis.thieman@gmail.com>
2015-05-29 13:39:16 -0400

    Add dusty script command

---
+++ dusty/cli/script.py
@@ -0,0 +1,24 @@
+"""Execute scripts defined in an app's spec inside a running app container.
+
+Usage:
+  script <app_name> [<script_name>]
+
+Examples:
+  To get information on all scripts available for an app called `website`:
+    dusty script website
+
+  To run the `rebuild` script defined inside the `website` app spec:
+    dusty script website rebuild
+"""
+
+from docopt import docopt
+
+from ..payload import Payload
+from ..commands.script import script_info_for_app, execute_script
+
+def main(argv):
+    args = docopt(__doc__, argv)
+    if not args['<script_name>']:
+        return Payload(script_info_for_app, args['<app_name>'])
+    else:
+        return execute_script(args['<app_name>'], args['<script_name>'])
---
+++ dusty/commands/script.py
@@ -0,0 +1,31 @@
+import textwrap
+
+from prettytable import PrettyTable
+
+from ..log import log_to_client
+from ..compiler.spec_assembler import get_specs
+from .utils import exec_docker
+
+def script_info_for_app(app_name):
+    app_specs = get_specs()['apps'].get(app_name)
+    if not app_specs:
+        raise KeyError('No app found named {} in specs'.format(app_name))
+    if not app_specs.get('scripts'):
+        log_to_client('No scripts registered for app {}'.format(app_name))
+        return
+
+    table = PrettyTable(['Script', 'Description'])
+    for script_name, script_spec in app_specs['scripts'].iteritems():
+        table.add_row([script_name,
+                       '\n'.join(textwrap.wrap(script_spec.get('description', ''), 80))])
+    log_to_client(table.get_string(sortby='Script'))
+
+def execute_script(app_name, script_name):
+    app_specs = get_specs()['apps'].get(app_name)
+    if not app_specs:
+        raise KeyError('No app found named {} in specs'.format(app_name))
+    if 'scripts' not in app_specs or script_name not in app_specs['scripts']:
+        raise KeyError('No script found named {} in specs for app {}'.format(script_name, app_name))
+
+    container_name = 'dusty_{}_1'.format(app_name)
+    exec_docker('exec', container_name, 'sh', '-c', app_specs['scripts'][script_name]['command'])
