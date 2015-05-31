f3473508f6310fc5dfb9e00d3a1786996d43de40
Travis Thieman <travis.thieman@gmail.com>
2015-05-31 11:38:18 -0400

    Additional tests for cp CLI

--- tests/cli/cp_test.py
+++ tests/cli/cp_test.py
@@ -1,9 +1,11 @@
+import os
+
 from mock import patch
 
 from ..utils import DustyTestCase
 from dusty.payload import Payload
 from dusty.commands.cp import copy_between_containers, copy_from_local, copy_to_local
-from dusty.cli.cp import _split_path, main
+from dusty.cli.cp import _split_path, _resolve_path, _validate_path_pair, main
 
 class TestCpCLI(DustyTestCase):
     def test_split_path_no_name(self):
@@ -15,6 +17,25 @@ class TestCpCLI(DustyTestCase):
     def test_split_too_many_colons(self):
         with self.assertRaises(ValueError):
             _split_path('website:api:/tmp/path.txt')
+
+    def test_resolve_path_absolute(self):
+        self.assertEqual(_resolve_path('/tmp/a'), '/tmp/a')
+
+    def test_resolve_path_relative(self):
+        self.assertEqual(_resolve_path('a'), os.path.join(os.getcwd(), 'a'))
+
+    def test_validate_path_pair_success_local_absolute(self):
+        _validate_path_pair(None, '/tmp/a')
+
+    def test_validate_path_pair_success_local_relative(self):
+        _validate_path_pair(None, 'a')
+
+    def test_validate_path_pair_success_container(self):
+        _validate_path_pair('some-container', '/tmp/a')
+
+    def test_validate_path_pair_relative_in_container(self):
+        with self.assertRaises(RuntimeError):
+            _validate_path_pair('some-container', 'a')
 
     def test_main_local_to_container(self):
         result = main(['/tmp/a', 'website:/tmp/b'])
