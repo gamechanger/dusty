63ef2be6bb8785bda5f827a20386195ad61fd3bd
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-06-01 15:01:32 -0400

    slightly fleshed out

--- dusty/cli/__init__.py
+++ dusty/cli/__init__.py
@@ -17,7 +17,7 @@ Commands:
   stop       Stop Dusty-managed containers
   sync       Sync repos from the local OS to the boot2docker VM
   up         Set up the Dusty environment and start activated applications
-  validate   Validates Dusty specs
+  validate   Validates that Dusty specs have correct fields and references
 
 For help on a specific command, provide the '-h' flag to the command, e.g. 'dusty repos -h'
 """
