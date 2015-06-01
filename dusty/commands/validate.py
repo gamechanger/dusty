70ff378f984bea9484b02ac1b3403c556fc3dc6d
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro-3.local>
2015-06-01 11:32:11 -0400

    started checking refs

--- dusty/commands/validate.py
+++ dusty/commands/validate.py
@@ -20,8 +20,24 @@ def _validate_fields_with_schemer(specs):
     for lib in specs.get('libs', []).values():
         lib_schema.validate(lib)
 
-def _validate_spec_names(specs):
+def _validate_app_references(app, specs):
+    for spec_type in ['apps', 'libs', 'services']:
+        dependent = app.get('depends', {}).get(spec_type, []) + app.get('conditional_links', {}).get(spec_type, [])
+        assert(all(spec_name in specs.get(spec_type, {}).keys() for spec_name in dependent))
+
+def _validate_bundle_references(bundle, specs):
     pass
+
+def _validate_lib_references(lib, specs):
+    pass
+
+def _validate_spec_names(specs):
+    for app in specs.get('apps', {}).values():
+        _validate_app_references(app, specs)
+    for bundle in specs.get('bundles', {}).values():
+        _validate_bundle_references(bundle, specs)
+    for lib in specs.get('libs', {}).values():
+        _validate_lib_references(lib, specs)
 
 def _validate_cycle_free(specs):
     pass
