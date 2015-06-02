cbbd7c9f4e539840a0066c20b2a865f61dd3d201
paetling <paetling@gmail.com>
2015-06-02 15:02:29 -0400

    make the nginx process a little nicer

--- dusty/commands/setup.py
+++ dusty/commands/setup.py
@@ -3,12 +3,12 @@ import subprocess
 
 from ..payload import Payload
 from .. import constants
-from ..config import save_config_value
+from ..config import save_config_value, get_config_value
 from ..log import log_to_client
 
 def _get_mac_username():
     proposed_mac_username = subprocess.check_output(['id', '-un']).strip()
-    if raw_input('Is {} your mac_username. Enter y for yes: '.format(proposed_mac_username)).upper() == 'Y':
+    if raw_input('Is {} your mac_username. y or no: '.format(proposed_mac_username)).strip().upper() == 'Y':
         return proposed_mac_username
     else:
         return raw_input('Enter your actual mac_username: ').strip()
@@ -17,7 +17,10 @@ def _get_default_specs_repo():
     return raw_input('Please input the repo of your specs repo. For example github.com/gamechanger/dusty: ').strip()
 
 def _get_nginx_includes_dir():
-    return raw_input('Please input the path where your nginx config pulls extra configs. The default value is /usr/local/etc/nginx/servers: ').strip()
+    default_nginx_config_value = get_config_value('nginx_includes_dir')
+    if raw_input('Does your nginx config look for extra configs in the default location of {}. y or n: '.format(default_nginx_config_value)).strip().upper() == 'Y':
+        return default_nginx_config_value
+    return raw_input('Please input the path where your nginx config pulls extra configs: ').strip()
 
 def setup_dusty_config():
     logging.info('Going to be setting values for these config values: {}'.format(constants.WARN_ON_MISSING_CONFIG_KEYS))
