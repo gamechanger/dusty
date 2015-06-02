b199e2073054d8ce9648df93b7232faf64985795
Zachary Markin <zach.markin@gamechanger.io>
2015-06-02 16:22:38 -0400

    kill newline

--- dusty/commands/utils.py
+++ dusty/commands/utils.py
@@ -8,7 +8,6 @@ from ..systems.docker import get_docker_env
 def _executable_path(executable_name):
     return subprocess.check_output(['which', executable_name]).strip()
 
-
 def exec_docker(*args):
     updated_env = copy(os.environ)
     updated_env.update(get_docker_env())
