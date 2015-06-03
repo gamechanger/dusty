5e505faeb693d961a5b764cfe4be1c975a9be926
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-03 15:40:12 -0400

    fixes to creation script

--- setup/create_binaries.sh
+++ setup/create_binaries.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
-venv_name=vent
+venv_name=venv
 rm -rf $venv_name
 virtualenv $venv_name
 python setup.py install
-$venv_name/bin/pip install pyinstaller
+$venv_name/bin/pip install "git+https://github.com/pyinstaller/pyinstaller.git@12e40471c77f588ea5be352f7219c873ddaae056#egg=pyinstaller"
 $venv_name/bin/pip install .
 $venv_name/bin/pyinstaller -F setup/bin/dusty
-$venv_name/pyinstaller -F setup/bin/dustyd
+$venv_name/bin/pyinstaller -F setup/bin/dustyd
 rm -rf $venv_name
