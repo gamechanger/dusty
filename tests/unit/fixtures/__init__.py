95278efe0a88890af135506c2b32bdcc791cb82d
Travis Thieman <travis.thieman@gmail.com>
2015-06-03 11:26:46 -0400

    Start adding integration tests for CLI

--- tests/unit/fixtures/__init__.py
+++ tests/unit/fixtures/__init__.py
@@ -3,6 +3,11 @@ import yaml
 
 from dusty.compiler.spec_assembler import get_specs_path
 
+def _num_to_alpha(num):
+    if num >= 26:
+        raise ValueError('Only supports up to 26')
+    return chr(num + 97)
+
 def _write(spec_type, name, spec_doc):
     spec_type_path = os.path.join(get_specs_path(), '{}s'.format(spec_type))
     try:
@@ -22,3 +27,9 @@ def basic_specs_fixture():
     _write('app', 'app-b', {'repo': 'github.com/app/b', 'image': 'app/b'})
     _write('lib', 'lib-a', {'repo': 'github.com/lib/a', 'image': 'lib/a'})
     _write('service', 'service-a', {'image': 'service/a'})
+
+def busybox_single_app_fixture(num_bundles=1):
+    for bundle in range(num_bundles):
+        name = 'busybox-{}'.format(_num_to_alpha(bundle))
+        _write('bundle', name, {'description': 'Busybox bundle', 'apps': [name]})
+        _write('app', name, {'repo': 'github.com/fake/repo', 'image': 'busybox'})
--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -45,3 +45,48 @@ class DustyTestCase(TestCase):
     @property
     def last_client_output(self):
         return self.client_output[-1] if self.client_output else None
+
+class DustyIntegrationTestCase(TestCase):
+    """This test case intentionally avoids mocking whenever possible
+    in order to get as close as possible to the actual state that
+    would be experienced on a system running Dusty. Therefore,
+    integration tests are possibly destructive if run on a user's
+    machine. To help protect the user from running them accidentally,
+    integration tests will refuse to run unless the environment
+    variable DUSTY_ALLOW_INTEGRATION_TESTS is set.
+
+    Note that this also assumes it is running against an actual
+    Dustyd process on the local host."""
+    def setUp(self):
+        if not os.getenv('DUSTY_ALLOW_INTEGRATION_TESTS'):
+            raise RuntimeError('You must set the env var DUSTY_ALLOW_INTEGRATION_TESTS to run integration tests. '
+                               'This may affect your local config, do not run integration tests on your actual '
+                               "machine unless you know what you're doing!")
+        self.overridden_specs_path = tempfile.mkdtemp()
+        write_default_config()
+        save_config_value('specs_repo', 'github.com/gamechanger/example-dusty-specs')
+        override_repo(get_specs_repo(), self.overridden_specs_path)
+
+    def tearDown(self):
+        shutil.rmtree(self.overridden_specs_path)
+
+    @property
+    def stdout(self):
+        return sys.stdout.getvalue().strip()
+
+    @patch('sys.exit')
+    def run_command(self, args, fake_exit):
+        sys.argv = ['dusty'] + args.split(' ')
+        client_entrypoint()
+
+    def _in_same_line(self, *values):
+        for line in self.stdout.splitlines():
+            if all(value in line for value in values):
+                return True
+        return False
+
+    def assertInSameLine(self, *values):
+        self.assertTrue(self._in_same_line(*values))
+
+    def assertNotInSameLine(self, *values):
+        self.assertFalse(self._in_same_line(*values))
