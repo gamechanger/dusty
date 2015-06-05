f083c5ff8066ffabc7919185297ecdc28060a448
Zachary Markin <zach.markin@gamechanger.io>
2015-06-04 23:13:22 -0400

    add --tail NUM lines to logs command

--- dusty/commands/logs.py
+++ dusty/commands/logs.py
@@ -1,8 +1,9 @@
-import os
-
 from ..systems.docker import get_dusty_containers
 from .utils import exec_docker
 
-def tail_container_logs(app_or_service_name):
+def tail_container_logs(app_or_service_name, lines=None):
     container = get_dusty_containers([app_or_service_name])[0]
-    exec_docker('logs', '-f', container['Id'])
+    if lines is None:
+        exec_docker('logs', '-f', container['Id'])
+    else:
+        exec_docker('logs', '-f', '--tail={}'.format(lines), container['Id'])
