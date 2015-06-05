643fc275b8685fd53ceb39ff57b7c1952fdb9a1d
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-05 14:18:04 -0400

    bold_echo function

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,13 @@
 release=0.0.1
-echo -e "\033[1mDownloading dusty files\033[0m"
+function bold_echo {
+    echo -e "\033[1m$1\033[0m"
+}
+bold_echo "Downloading dusty files"
 curl -L https://github.com/gamechanger/dusty/releases/download/$release/dusty > /usr/local/bin/dusty
 curl -L https://github.com/gamechanger/dusty/releases/download/$release/dustyd > /usr/local/bin/dustyd
-echo -e "\033[1mAuthenticating as super user... needed to setup daemon\033[0m"
+bold_echo "Authenticating as super user... needed to setup daemon"
 sudo -v
 sudo curl -L -o /System/Library/LaunchDaemons/org.gamechanger.dustyd.plist https://raw.githubusercontent.com/gamechanger/dusty/master/setup/org.gamechanger.dustyd.plist
-echo -e "\033[1mAdding & resetting dustyd daemon\033[0m"
+bold_echo "Adding & resetting dustyd daemon"
 sudo launchctl unload /System/Library/LaunchDaemons/org.gamechanger.dustyd.plist
 sudo launchctl load /System/Library/LaunchDaemons/org.gamechanger.dustyd.plist
