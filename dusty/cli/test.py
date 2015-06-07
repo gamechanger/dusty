1fa6f3344a263069d63c0c16192f23d58c1fd91f
paetling <paetling@gmail.com>
2015-06-07 16:05:54 -0400

    add in new files

---
+++ dusty/cli/test.py
@@ -0,0 +1,17 @@
+"""Allow you to run tests in an isolated container for an app or a lib
+
+Usage:
+  test create_image <app_or_lib_name> [--recreate]
+
+Options:
+  --recreate  ensures that testing image will be recreated
+
+"""
+
+from docopt import docopt
+
+from ..commands.test import test_app_or_lib
+
+def main(argv):
+    args = docopt(__doc__, argv)
+    test_app_or_lib(args['<app_or_lib_name>'], force_recreate=args['--recreate'])
---
+++ dusty/commands/test.py
@@ -0,0 +1,19 @@
+from ..compiler.spec_assembler import get_expanded_libs_specs
+from ..compiler.compose import get_app_volume_mounts, get_lib_volume_mounts
+from ..systems.docker.testing_image import ensure_image_exists
+
+
+def test_app_or_lib(app_or_lib_name, force_recreate=False):
+    expanded_specs = get_expanded_libs_specs()
+    if app_or_lib_name in expanded_lib_specs['apps']:
+        volumes = get_app_volume_mounts(app_or_lib_name, expanded_specs)
+        testing_spec = expanded_specs['apps'][app_or_lib_name]['test']
+    elif app_or_lib_name in expanded_lib_specs['libs']:
+        volumes = get_lib_volume_mounts(app_or_lib_name, expanded_specs)
+        testing_spec = expanded_specs['libs'][app_or_lib_name]['test']
+    else:
+        raise KeyError('Argument must be app or lib name')
+
+    image_name = "{}_dusty_testing/image".format(app_or_lib_name)
+    ensure_image_exists(testing_spec, image_name, volumes=volumes, force_recreate=force_recreate)
+
