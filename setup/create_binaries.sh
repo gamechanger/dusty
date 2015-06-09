9ea391c3b834504e0be8ea59bef3da8f37ea4d07
Travis Thieman <travis.thieman@gmail.com>
2015-06-09 14:30:54 -0400

    Try hitting both entrypoints for imports

--- setup/create_binaries.sh
+++ setup/create_binaries.sh
@@ -6,6 +6,6 @@ virtualenv $venv_name
 $venv_name/bin/pip install "git+https://github.com/pyinstaller/pyinstaller.git@12e40471c77f588ea5be352f7219c873ddaae056#egg=pyinstaller"
 $venv_name/bin/pip install .
 $venv_name/bin/pyinstaller -F setup/bin/dusty
-$venv_name/bin/pyinstaller -F --hidden-import=dusty.commands setup/bin/dustyd
+$venv_name/bin/pyinstaller -F --hidden-import=dusty.cli setup/bin/dustyd
 echo "Binaries can now be found at dist/dusty and dist/dustyd"
 rm -rf $venv_name
