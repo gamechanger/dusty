cfff359877c25517519a1fea53956ec4554fabf5
Travis Thieman <travis.thieman@gmail.com>
2015-06-10 17:26:12 -0400

    Add new parameter to the mocks

--- tests/unit/commands/logs_test.py
+++ tests/unit/commands/logs_test.py
@@ -9,7 +9,7 @@ class TestLogsCommands(DustyTestCase):
     def test_tail_container_logs(self, fake_get_containers, fake_exec_docker):
         fake_get_containers.return_value = [{'Id': 'container-id'}]
         tail_container_logs('app-a')
-        fake_get_containers.assert_called_once_with(['app-a'])
+        fake_get_containers.assert_called_once_with(['app-a'], include_exited=True)
         fake_exec_docker.assert_called_once_with('logs', 'container-id')
 
     @patch('dusty.commands.logs.exec_docker')
@@ -17,7 +17,7 @@ class TestLogsCommands(DustyTestCase):
     def test_tail_with_line_number(self, fake_get_containers, fake_exec_docker):
         fake_get_containers.return_value = [{'Id': 'container-id'}]
         tail_container_logs('app-a', lines=4)
-        fake_get_containers.assert_called_once_with(['app-a'])
+        fake_get_containers.assert_called_once_with(['app-a'], include_exited=True)
         fake_exec_docker.assert_called_once_with('logs', '--tail=4', 'container-id')
 
     @patch('dusty.commands.logs.exec_docker')
@@ -25,13 +25,13 @@ class TestLogsCommands(DustyTestCase):
     def test_tail_container_logs_with_follow(self, fake_get_containers, fake_exec_docker):
         fake_get_containers.return_value = [{'Id': 'container-id'}]
         tail_container_logs('app-a', follow=True)
-        fake_get_containers.assert_called_once_with(['app-a'])
+        fake_get_containers.assert_called_once_with(['app-a'], include_exited=True)
         fake_exec_docker.assert_called_once_with('logs', '-f', 'container-id')
 
     @patch('dusty.commands.logs.exec_docker')
     @patch('dusty.commands.logs.get_dusty_containers')
-    def test_tail_with_line_number(self, fake_get_containers, fake_exec_docker):
+    def test_tail_with_line_number_and_follow(self, fake_get_containers, fake_exec_docker):
         fake_get_containers.return_value = [{'Id': 'container-id'}]
         tail_container_logs('app-a', follow=True, lines=4)
-        fake_get_containers.assert_called_once_with(['app-a'])
+        fake_get_containers.assert_called_once_with(['app-a'], include_exited=True)
         fake_exec_docker.assert_called_once_with('logs', '-f', '--tail=4', 'container-id')
