70e03f2d3f6f56af6a59c4b80c7aab01d70b3af2
Zachary Markin <zach.markin@gamechanger.io>
2015-06-11 14:40:33 -0400

    update and add tests

--- tests/integration/cli/stop_test.py
+++ tests/integration/cli/stop_test.py
@@ -4,8 +4,8 @@ from ...fixtures import busybox_single_app_bundle_fixture
 class TestStopCLI(DustyIntegrationTestCase):
     def setUp(self):
         super(TestStopCLI, self).setUp()
-        busybox_single_app_bundle_fixture(num_bundles=1)
-        self.run_command('bundles activate busyboxa')
+        busybox_single_app_bundle_fixture(num_bundles=3)
+        self.run_command('bundles activate busyboxa busyboxb busyboxc')
         self.run_command('up')
 
     def tearDown(self):
@@ -22,8 +22,37 @@ class TestStopCLI(DustyIntegrationTestCase):
         self.assertContainerExists('busyboxa')
 
     def test_stop_container_with_rm_flag(self):
-        self.run_command('restart')
         self.assertContainerRunning('busyboxa')
         self.run_command('stop --rm')
         self.assertContainerIsNotRunning('busyboxa')
         self.assertContainerDoesNotExist('busyboxa')
+
+    def test_stop_only_one(self):
+        self.assertContainerRunning('busyboxa')
+        self.assertContainerRunning('busyboxb')
+        self.run_command('stop busyboxa')
+        self.assertContainerIsNotRunning('busyboxa')
+        self.assertContainerRunning('busyboxb')
+
+    def test_stop_multiple_but_not_all(self):
+        self.assertContainerRunning('busyboxa')
+        self.assertContainerRunning('busyboxb')
+        self.assertContainerRunning('busyboxc')
+        self.run_command('stop busyboxa busyboxb')
+        self.assertContainerIsNotRunning('busyboxa')
+        self.assertContainerIsNotRunning('busyboxb')
+        self.assertContainerRunning('busyboxc')
+
+    def test_stop_with_rm(self):
+        self.assertContainerRunning('busyboxa')
+        self.run_command('stop --rm busyboxa')
+        self.assertContainerDoesNotExist('busyboxa')
+
+    def test_stop_multiple_but_not_all_with_rm(self):
+        self.assertContainerRunning('busyboxa')
+        self.assertContainerRunning('busyboxb')
+        self.assertContainerRunning('busyboxc')
+        self.run_command('stop --rm busyboxa busyboxb')
+        self.assertContainerDoesNotExist('busyboxa')
+        self.assertContainerDoesNotExist('busyboxb')
+        self.assertContainerRunning('busyboxc')
