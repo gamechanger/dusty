5e39b6e5629c45359f90d1a2efcb4c8ac3037156
Travis Thieman <travis.thieman@gmail.com>
2015-06-11 13:33:16 -0400

    Write testing Composefile to user home for now

--- dusty/commands/test.py
+++ dusty/commands/test.py
@@ -1,3 +1,6 @@
+import os
+
+from .. import constants
 from ..compiler.spec_assembler import get_expanded_libs_specs
 from ..compiler.compose import get_app_volume_mounts, get_lib_volume_mounts, get_testing_compose_dict
 from ..systems.docker.testing_image import ensure_image_exists
@@ -37,6 +40,9 @@ def _construct_test_command(spec, suite_name, test_arguments):
     log_to_client('Command to run in test is {}'.format(test_command))
     return test_command
 
+def _test_composefile_path(service_name):
+    return os.path.expanduser('~/.dusty-testing/test_{}.yml'.format(service_name))
+
 def _services_compose_up(expanded_specs, app_or_lib_spec):
     previous_container_names = []
     for service_name in app_or_lib_spec['test']['services']:
@@ -47,11 +53,11 @@ def _services_compose_up(expanded_specs, app_or_lib_spec):
         service_compose_config = get_testing_compose_dict(service_name, service_spec, **kwargs)
 
         #want to make these temporary files
-        compose_file = '/Users/paetling/dusty_testing/service_{}'.format(service_name)
-        write_composefile(service_compose_config, compose_file)
+        composefile_path = _test_composefile_path(service_name)
+        write_composefile(service_compose_config, composefile_path)
         log_to_client('Compose config {}: \n {}'.format(service_name, service_compose_config))
 
-        compose_up(compose_file, service_name)
+        compose_up(composefile_path, service_name)
         #compose only has runs with lower case names
         previous_container_names.append("{}_{}_1".format(service_name.lower(), service_name))
     return previous_container_names
@@ -64,21 +70,16 @@ def _app_or_lib_compose_up(app_or_lib_spec, app_or_lib_name, image_name,
 
     if previous_container_name is not None:
         kwargs['net_container_identifier'] = previous_container_name
-    compose_file = '/Users/paetling/dusty_testing/service_{}'.format(app_or_lib_name)
+    composefile_path = _test_composefile_path(app_or_lib_name)
     compose_config = get_testing_compose_dict(app_or_lib_name, app_or_lib_spec['test'].get('compose', {}), **kwargs)
-    write_composefile(compose_config, compose_file)
-    compose_up(compose_file, app_or_lib_name)
+    write_composefile(compose_config, composefile_path)
+    compose_up(composefile_path, app_or_lib_name)
     log_to_client('Compose config {}: \n {}'.format(app_or_lib_name, compose_config))
 
 def _run_tests_with_image(expanded_specs, app_or_lib_name, app_or_lib_spec, app_or_lib_volumes, image_name, test_command):
     log_to_client('image name is {}'.format(image_name))
 
-
     previous_container_names = _services_compose_up(expanded_specs, app_or_lib_spec)
     previous_container_name = previous_container_names[-1] if previous_container_names else None
-    _app_or_lib_compose_up(app_or_lib_spec, app_or_lib_name, image_name, app_or_lib_volumes, test_command, previous_container_name)
-
-
-    #need to wait on tests finishing and cleanup shit afterwards
-
-
+    _app_or_lib_compose_up(app_or_lib_spec, app_or_lib_name, image_name,
+                           app_or_lib_volumes, test_command, previous_container_name)
