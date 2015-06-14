9393718b8cee813ed74b22dcd76eb53b64ce2e84
paetling <paetling@gmail.com>
2015-06-14 17:58:47 -0400

    show the default args in pretty table for suites and make more clear that default args are dropped

--- dusty/cli/test.py
+++ dusty/cli/test.py
@@ -1,4 +1,5 @@
-"""Allow you to run tests in an isolated container for an app or a lib
+"""Allow you to run tests in an isolated container for an app or a lib.
+If args are passed, default arguments are dropped
 
 Usage:
   test <app_or_lib_name> [<suite_name>] [<args>...] [--recreate]
@@ -7,6 +8,12 @@ Options:
   <suite_name>  Name of the test suite you would like to run
   <args>        A list of arguments to be passed to the test script
   --recreate    Ensures that the testing image will be recreated
+
+Examples:
+  To call test suite frontend with default arguments:
+    dusty test web frontend
+  To call test suite frontend with arguments in place of the defaults:
+    dusty test web frontend /web/javascript
 
 """
 
