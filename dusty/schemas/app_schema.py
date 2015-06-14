cc6f861b161cc4bff60d118ad3e9110c1ac7fb6f
Travis Thieman <travis.thieman@gmail.com>
2015-06-13 21:25:12 -0400

    Allow default_args in test spec, also add test schema

--- dusty/schemas/app_schema.py
+++ dusty/schemas/app_schema.py
@@ -1,5 +1,4 @@
 from schemer import Schema, Array
-
 def image_build_isolation_validator():
     def validator(document):
         if 'image' in document and 'build' in document:
@@ -40,7 +39,20 @@ dusty_app_compose_schema = Schema({
     'volumes': {'type': Array(basestring), 'default': list}
     }, strict=False)
 
-test_schema = Schema({}, strict=False)
+test_suite_schema = Schema({
+    'name': {'type': basestring, 'required': True},
+    'command': {'type': basestring, 'required': True},
+    'default_args': {'type': basestring, 'required': False, 'default': ''}
+})
+
+test_schema = Schema({
+    'image': {'type': basestring, 'default': ''},
+    'build': {'type': basestring},
+    'services': {'type': Array(basestring), 'default': list},
+    'once': {'type': basestring, 'required': True},
+    'compose': {'type': dusty_app_compose_schema, 'default': dict},
+    'suites': {'type': Array(test_suite_schema), 'default': list},
+    }, strict=False)
 
 app_schema = Schema({
     'repo': {'type': basestring, 'required': True},
