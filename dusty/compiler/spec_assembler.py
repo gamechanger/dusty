63ba8d5ede8a5da94cbcb43e182d230022833b05
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-16 13:13:00 -0400

    refactor classes

--- dusty/compiler/spec_assembler.py
+++ dusty/compiler/spec_assembler.py
@@ -172,8 +172,7 @@ def get_all_repos(active_only=False, include_specs_repo=True):
     if include_specs_repo:
         repos.add(get_specs_repo())
     specs = get_assembled_specs() if active_only else get_specs()
-    for type_key in ['apps', 'libs']:
-        for spec in specs[type_key].itervalues():
-            if 'repo' in spec:
-                repos.add(Repo(spec['repo']))
+    for spec in specs.get_apps_and_libs():
+        if 'repo' in spec:
+            repos.add(Repo(spec['repo']))
     return repos
--- dusty/schemas/base_schema_class.py
+++ dusty/schemas/base_schema_class.py
@@ -1,12 +1,10 @@
 from copy import deepcopy
 import collections
 
-# This is build on top of Schemer's functionality
-class DustySchema(collections.MutableMapping):
-    def __init__(self, schema, document):
-        schema.validate(document)
-        self._document = deepcopy(document)
-        schema.apply_defaults(self._document)
+
+class BaseMutable(collections.MutableMapping):
+    def __init__(self, document):
+        self._document = document
 
     def __getitem__(self, name):
         return self._document[name]
@@ -33,37 +31,22 @@ class DustySchema(collections.MutableMapping):
         return self._document.values()
 
 
-class DustySpecs(collections.MutableMapping):
-    def __init__(self, specs_dict):
-        self._specs_dict = specs_dict
+# This is build on top of Schemer's functionality
+class DustySchema(BaseMutable):
+    def __init__(self, schema, document):
+        schema.validate(document)
+        self._document = deepcopy(document)
+        super(DustySchema, self).__init__(deepcopy(document))
+        schema.apply_defaults(self._document)
 
-    def __getitem__(self, name):
-        return self._specs_dict[name]
 
-    def __setitem__(self, key, value):
-        self._specs_dict[key] = value
-
-    def __delitem__(self, key):
-        del self._specs_dict[key]
-
-    def __contains__(self, key):
-        return key in self._specs_dict
-
-    def __iter__(self):
-        return iter(self._specs_dict)
-
-    def __len__(self):
-        return len(self._specs_dict)
-
-    def keys(self):
-        return self._specs_dict.keys()
-
-    def values(self):
-        return self._specs_dict.values()
-
+class DustySpecs(BaseMutable):
     def get_app_or_lib(self, app_or_lib_name):
-        if app_or_lib_name in self._specs_dict['apps']:
-            return self._specs_dict['apps'][app_or_lib_name]
-        elif app_or_lib_name in self._specs_dict['libs']:
-            return self._specs_dict['libs'][app_or_lib_name]
+        if app_or_lib_name in self._document['apps']:
+            return self._document['apps'][app_or_lib_name]
+        elif app_or_lib_name in self._document['libs']:
+            return self._document['libs'][app_or_lib_name]
         raise KeyError('did not find app or service with name {}'.format(app_or_library_name))
+
+    def get_apps_and_libs(self):
+        return [app for app in self._document['apps'].values()] + [lib for lib in self._document['libs'].values()]
