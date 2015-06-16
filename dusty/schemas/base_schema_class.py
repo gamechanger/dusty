031b1ea43ac214ee64469769ac60f123962a9e28
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-16 11:54:25 -0400

    use abstract base class

--- dusty/schemas/base_schema_class.py
+++ dusty/schemas/base_schema_class.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
+import collections
 
 # This is build on top of Schemer's functionality
-class DustySchema(object):
+class DustySchema(collections.MutableMapping):
     def __init__(self, schema, document):
         schema.validate(document)
         self._document = deepcopy(document)
@@ -19,8 +20,43 @@ class DustySchema(object):
     def __contains__(self, key):
         return key in self._document
 
+    def __iter__(self):
+        return iter(self._document)
+
+    def __len__(self):
+        return len(self._document)
+
     def keys(self):
         return self._document.keys()
 
     def values(self):
         return self._document.values()
+
+
+class DustySpecs(collections.MutableMapping):
+    def __init__(self, specs_dict):
+        self._specs_dict = specs_dict
+
+    def __getitem__(self, name):
+        return self._specs_dict[name]
+
+    def __setitem__(self, key, value):
+        self._specs_dict[key] = value
+
+    def __delitem__(self, key):
+        del self._specs_dict[key]
+
+    def __contains__(self, key):
+        return key in self._specs_dict
+
+    def __iter__(self):
+        return iter(self._specs_dict)
+
+    def __len__(self):
+        return len(self._specs_dict)
+
+    def keys(self):
+        return self._specs_dict.keys()
+
+    def values(self):
+        return self._specs_dict.values()
