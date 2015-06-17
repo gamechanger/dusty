4a49571f418535f35ebef070d243bbff65ab61ef
paetling <paetling@gmail.com>
2015-06-17 18:09:31 -0400

    only get if not overridden

--- dusty/commands/repos.py
+++ dusty/commands/repos.py
@@ -46,8 +46,9 @@ def update_managed_repos():
     log_to_client('Pulling latest updates for all active managed repos:')
     overrides = set(get_config_value(constants.CONFIG_REPO_OVERRIDES_KEY))
     specs_repo = get_specs_repo()
-    log_to_client('Updating managed copy of specs-repo before loading specs')
-    specs_repo.update_local_repo()
+    if not specs_repo.is_overridden:
+        log_to_client('Updating managed copy of specs-repo before loading specs')
+        specs_repo.update_local_repo()
     for repo in get_all_repos(active_only=True, include_specs_repo=False):
         if not repo.is_overridden:
             log_to_client('Updating managed copy of {}'.format(repo.remote_path))
