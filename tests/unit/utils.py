b0b8bd29dd228c03e1a8dbf353fe5f2ba47a496f
Travis Thieman <travis.thieman@gmail.com>
2015-06-19 12:00:49 -0400

    Update test utils for new optional schema

--- tests/unit/utils.py
+++ tests/unit/utils.py
@@ -6,10 +6,10 @@ from dusty.schemas.bundle_schema import bundle_schema
 def get_app_dusty_schema(doc, name=None):
     if 'image' not in doc and 'build' not in doc:
         doc['image'] = ''
-    if 'repo' not in doc:
-        doc['repo'] = ''
-    if 'mount' not in doc:
-        doc['mount'] = ''
+    if 'repo' in doc and 'mount' not in doc:
+        doc['mount'] = '/repo'
+    if 'mount' in doc and 'repo' not in doc:
+        doc['repo'] = '/repo'
     return DustySchema(app_schema, doc, name, 'apps')
 
 def get_lib_dusty_schema(doc, name=None):
