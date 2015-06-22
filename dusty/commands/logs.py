6a571cf9bb36d5e0b7094a3f877f6d544d4db2fd
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-22 17:33:43 -0400

    better logging on a random error case

--- dusty/commands/logs.py
+++ dusty/commands/logs.py
@@ -1,8 +1,14 @@
+import logging
+
 from ..systems.docker import get_dusty_containers
 from . import utils
 
 def tail_container_logs(app_or_service_name, follow=False, lines=None):
-    container = get_dusty_containers([app_or_service_name], include_exited=True)[0]
+    containers = get_dusty_containers([app_or_service_name], include_exited=True)
+    if len(containers) == 0:
+        logging.info('No container exists which corresponds to {}'.format(app_or_service_name))
+        return
+    container = containers[0]
     args = ['logs']
     if follow:
         args.append('-f')
