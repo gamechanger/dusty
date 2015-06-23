9a40e94d64606ce73cc0eed2e4af2b1bcb9cfc68
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-23 17:20:23 -0400

    use a os.path.join with a strip

--- dusty/constants.py
+++ dusty/constants.py
@@ -28,7 +28,7 @@ NGINX_MAX_FILE_SIZE = "500M"
 
 VM_PERSIST_DIR = '/persist'
 VM_REPOS_DIR = os.path.join(VM_PERSIST_DIR, 'repos')
-LOCAL_BACKUP_DIR = '{}/backup{}'.format(CONFIG_DIR, VM_PERSIST_DIR)
+LOCAL_BACKUP_DIR = os.path.join(CONFIG_DIR, 'backup', VM_PERSIST_DIR.strip('/'))
 
 VM_CP_DIR = '/cp'
 CONTAINER_CP_DIR = '/cp'
