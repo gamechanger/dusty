640c1958ea5c45e5e1cdcb26520552e8cc81e763
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-23 17:46:59 -0400

    small stuff from comments

--- dusty/cli/restart.py
+++ dusty/cli/restart.py
@@ -9,13 +9,13 @@ Usage:
   restart ( --repos <repos>... | [<services>...] ) [--no-sync]
 
 Options:
-  --no-sync       If provided, Dusty will not sync repos used by
-                  services being restarted prior to the restart.
-  --repos <repos> If provided, Dusty will restart any containers
-                  that are using the repos specified.
-  <services>      If provided, Dusty will only restart the given
-                  services. Otherwise, all currently running
-                  services are restarted.
+  --no-sync         If provided, Dusty will not sync repos used by
+                    services being restarted prior to the restart.
+  --repos <repos>   If provided, Dusty will restart any containers
+                    that are using the repos specified.
+  <services>        If provided, Dusty will only restart the given
+                    services. Otherwise, all currently running
+                    services are restarted.
 """
 
 from docopt import docopt
@@ -25,6 +25,7 @@ from ..commands.run import restart_apps_or_services, restart_apps_by_repo
 
 def main(argv):
     args = docopt(__doc__, argv)
+    common_args = {'sync': not args['--no-sync']}
     if args['--repos']:
-        return Payload(restart_apps_by_repo, args['<repos>'], sync=not args['--no-sync'])
-    return Payload(restart_apps_or_services, args['<services>'], sync=not args['--no-sync'])
+        return Payload(restart_apps_by_repo, args['--repos'], **common_args)
+    return Payload(restart_apps_or_services, args['<services>'], **common_args)
