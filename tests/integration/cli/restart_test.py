5b173ebe70e514c5d4b0d6cca033014dd8915dcf
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-06-25 10:53:22 -0400

    restart all plus setup local repos

--- tests/integration/cli/restart_test.py
+++ tests/integration/cli/restart_test.py
@@ -3,6 +3,7 @@ import dateutil.parser
 
 from nose.tools import nottest
 
+from dusty.compiler.spec_assembler import get_all_repos
 from ...testcases import DustyIntegrationTestCase
 from ...fixtures import specs_fixture_with_depends
 
@@ -10,6 +11,8 @@ class TestRestartCli(DustyIntegrationTestCase):
     def setUp(self):
         super(TestRestartCli, self).setUp()
         specs_fixture_with_depends()
+        for repo in get_all_repos():
+            self._set_up_fake_local_repo(path=repo.local_path)
         self.run_command('bundles activate bundle-a bundle-b')
         self.run_command('up')
         self.up_complete_time = datetime.datetime.now()
@@ -30,3 +33,11 @@ class TestRestartCli(DustyIntegrationTestCase):
     def test_restart_one(self):
         self.run_command('restart app-a')
         self.assertTrue(self.container_has_restarted('app-a'))
+        self.assertTrue(not self.container_has_restarted('app-b'))
+        self.assertTrue(not self.container_has_restarted('app-c'))
+
+    def test_restart_all(self):
+        self.run_command('restart')
+        self.assertTrue(self.container_has_restarted('app-a'))
+        self.assertTrue(self.container_has_restarted('app-b'))
+        self.assertTrue(self.container_has_restarted('app-c'))
