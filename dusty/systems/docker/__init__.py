15a8968d3fc4e6a99aff50feb1f57d95fa974752
paetling <paetling@gmail.com>
2015-06-29 14:52:01 -0400

    have docker-py automatically determine version

--- dusty/systems/docker/__init__.py
+++ dusty/systems/docker/__init__.py
@@ -48,7 +48,8 @@ def get_docker_client():
     host, cert_path, tls_verify = env['DOCKER_HOST'], env['DOCKER_CERT_PATH'], env['DOCKER_TLS_VERIFY']
 
     params = {'base_url': host.replace('tcp://', 'https://'),
-              'timeout': None}
+              'timeout': None,
+              'version': 'auto'}
     if tls_verify and cert_path:
         params['tls'] = docker.tls.TLSConfig(
             client_cert=(os.path.join(cert_path, 'cert.pem'),
