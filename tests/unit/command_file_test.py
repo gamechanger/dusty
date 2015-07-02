59335c90841475fee4506b15701f8fa565d900b3
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-07-02 10:22:45 -0400

    attempt to fix old tests'

--- tests/unit/command_file_test.py
+++ tests/unit/command_file_test.py
@@ -89,14 +89,14 @@ class TestCommandFile(DustyTestCase):
                      '}',
                      'if [ ! -f {} ]'.format(constants.FIRST_RUN_FILE_PATH),
                      'then mkdir -p {}'.format(constants.RUN_DIR),
-                     'touch {}'.format(constants.FIRST_RUN_FILE_PATH),
-                     'dusty_once_fn | tee {}'.format(constants.ONCE_LOG_PATH),
+                     'touch {}'.format(constants.FIRST_RUN_FILE_PATH)
+                     ] + command_file._tee_output_commands('dusty_once_fn') + [
                      'fi',
                      'dusty_always_fn () {',
                      'app1 always 1',
                      'app1 always 2',
                      '}',
-                     'dusty_always_fn | tee {}'.format(constants.ALWAYS_LOG_PATH)]
+                     ] + command_file._tee_output_commands('dusty_always_fn')
         call1 = call(commands1, '{}/app1/dusty_command_file_app1.sh'.format(constants.COMMAND_FILES_DIR))
         commands2 = ['cd /gc/app1',
                      'script1 1',
@@ -204,7 +204,7 @@ class TestCommandFile(DustyTestCase):
             'if [ ! -f {} ]'.format(constants.FIRST_RUN_FILE_PATH),
             'then mkdir -p {}'.format(constants.RUN_DIR),
             'touch {}'.format(constants.FIRST_RUN_FILE_PATH),
-            'dusty_once_fn | tee {}'.format(constants.ONCE_LOG_PATH),
+            ] + command_file._tee_output_commands('dusty_once_fn') + [
             'fi'
         ]
         actual = command_file._get_once_commands(spec)
@@ -237,8 +237,7 @@ class TestCommandFile(DustyTestCase):
             'dusty_always_fn () {',
             'always_script.sh',
             '}',
-            'dusty_always_fn | tee {}'.format(constants.ALWAYS_LOG_PATH),
-        ]
+        ] + command_file._tee_output_commands('dusty_always_fn')
         actual = command_file._get_always_commands(spec)
         self.assertEqual(expected, actual)
 
