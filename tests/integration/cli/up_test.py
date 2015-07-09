ae16a72727c7f7d721b5f877d1b6e1a9b68073a0
paetling <paetling@gmail.com>
2015-07-09 16:33:35 -0400

    make this more sustainable by using docker_client and adding an ssert to INtegrationTestCase
    use preexisting function

--- tests/integration/cli/up_test.py
+++ tests/integration/cli/up_test.py
@@ -2,7 +2,6 @@ import subprocess
 
 from ...testcases import DustyIntegrationTestCase
 from ...fixtures import busybox_single_app_bundle_fixture
-from dusty.systems.docker import get_docker_env
 
 class TestUpCLI(DustyIntegrationTestCase):
     def setUp(self):
@@ -12,23 +11,21 @@ class TestUpCLI(DustyIntegrationTestCase):
 
     def test_basic_up_command(self):
         run_output = self.run_command('up')
-        self.assertTrue('Your local environment is now started!' in run_output)
-
-        docker_output = subprocess.check_output(['sh', '-c', "docker ps"], env=get_docker_env())
-        self.assertTrue('dusty_busyboxa_1' in docker_output)
-        self.assertTrue('dusty_busyboxb_1' in docker_output)
+        self.assertIn('Your local environment is now started!', run_output)
+        self.assertContainerRunning('busyboxa')
+        self.assertContainerRunning('busyboxb')
 
     def test_basic_up_recreate(self):
         run_output = self.run_command('up')
         run_output = self.run_command('up')
-        self.assertTrue('Recreating dusty_busyboxb_1' in run_output)
-        self.assertTrue('Recreating dusty_busyboxa_1' in run_output)
+        self.assertIn('Recreating dusty_busyboxb_1', run_output)
+        self.assertIn('Recreating dusty_busyboxa_1', run_output)
         run_output = self.run_command('up --no-recreate')
-        self.assertFalse('Recreating dusty_busyboxb_1' in run_output)
-        self.assertFalse('Recreating dusty_busyboxa_1' in run_output)
+        self.assertNotIn('Recreating dusty_busyboxb_1', run_output)
+        self.assertNotIn('Recreating dusty_busyboxa_1', run_output)
 
     def test_basic_up_no_pull(self):
         run_output = self.run_command('up')
-        self.assertTrue('Updating managed copy of /tmp/fake-repo' in run_output)
+        self.assertIn('Updating managed copy of /tmp/fake-repo', run_output)
         run_output = self.run_command('up --no-pull')
-        self.assertFalse('Updating managed copy of /tmp/fake-repo' in run_output)
+        self.assertNotIn('Updating managed copy of /tmp/fake-repo', run_output)
