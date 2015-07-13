62de9e441726114f380a7b6b01b4e3465c9e26e9
Travis Thieman <travis.thieman@gmail.com>
2015-07-13 12:40:42 -0400

    Move plist install dir to /Library/LaunchDaemons

--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,9 @@ set -e
 
 # This is set by Jenkins during release
 release=
+LEGACY_INSTALL_ROOT=/System/Library/LaunchDaemons
+INSTALL_ROOT=/Library/LaunchDaemons
+PLIST_NAME=org.gamechanger.dusty.plist
 
 function bold_echo {
     echo -e "\033[1m$1\033[0m"
@@ -13,9 +16,16 @@ chmod +x /usr/local/bin/dusty
 bold_echo "Authenticating as super user... needed to setup daemon"
 sudo -v
 bold_echo "Resetting dusty daemon"
-sudo curl -L -o /System/Library/LaunchDaemons/org.gamechanger.dusty.plist https://raw.githubusercontent.com/gamechanger/dusty/$release/setup/org.gamechanger.dusty.plist
-sudo launchctl unload /System/Library/LaunchDaemons/org.gamechanger.dusty.plist
+sudo curl -L -o $INSTALL_ROOT/$PLIST_NAME https://raw.githubusercontent.com/gamechanger/dusty/$release/setup/$PLIST_NAME
+sudo launchctl unload $INSTALL_ROOT/$PLIST_NAME
 bold_echo "Testing dusty daemon's preflight..."
 sudo -H dusty -d --preflight-only || (bold_echo "Preflight failed; not loading daemon"; exit 1)
 bold_echo "Loading dusty daemon"
-sudo launchctl load /System/Library/LaunchDaemons/org.gamechanger.dusty.plist
+sudo launchctl load $INSTALL_ROOT/$PLIST_NAME
+
+# Clean up install from legacy install directory if it exists
+if [ -f $LEGACY_INSTALL_ROOT/$PLIST_NAME ]; then
+    echo "Removing legacy install plist in $LEGACY_INSTALL_ROOT"
+    sudo launchctl unload $LEGACY_INSTALL_ROOT/$PLIST_NAME
+    sudo rm $LEGACY_INSTALL_ROOT/$PLIST_NAME
+fi
