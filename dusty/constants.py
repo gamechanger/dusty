01170da851376d361bbb24a6ca87aba0919085ab
Travis Thieman <travis.thieman@gmail.com>
2015-07-20 14:33:54 -0400

    Bump next version to 0.3.0 and note rsync change in changelog

--- dusty/constants.py
+++ dusty/constants.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = '0.2.3'
+VERSION = '0.3.0'
 BINARY = False # overridden by PyInstaller when we build a binary
 
 EXAMPLE_SPECS_REPO = 'github.com/gamechanger/dusty-example-specs'
