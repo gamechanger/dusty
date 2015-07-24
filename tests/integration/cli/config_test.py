806c4a5fc85c01107862ee82ed35262213e03bdb
Travis Thieman <travis.thieman@gmail.com>
2015-07-24 12:52:33 -0400

    This test needs string config values, apparently

--- tests/integration/cli/config_test.py
+++ tests/integration/cli/config_test.py
@@ -25,6 +25,6 @@ class TestConfigCLI(DustyIntegrationTestCase):
             self.run_command('config set bacon-level extreme')
 
     def test_config_set_works_with_valid_input(self):
-        self.run_command('config set setup_has_run you-betcha')
+        self.run_command('config set vm_memory_size 3000')
         result = yaml.load(self.run_command('config listvalues'))
-        self.assertEqual(result['setup_has_run'], 'you-betcha')
+        self.assertEqual(result['vm_memory_size'], '3000')
