d62bf6d4f395ccc072c74f8a6f3a5df3bb8ad781
Bartosz Grzybowski <melkorm@gmail.com>
2015-07-29 07:44:28 +0200

    Change default behaviour for timestamps to False

--- dusty/commands/logs.py
+++ dusty/commands/logs.py
@@ -3,7 +3,7 @@ import logging
 from ..systems.docker import get_dusty_containers
 from . import utils
 
-def tail_container_logs(app_or_service_name, follow=False, lines=None, timestamps=True):
+def tail_container_logs(app_or_service_name, follow=False, lines=None, timestamps=False):
     containers = get_dusty_containers([app_or_service_name], include_exited=True)
     if len(containers) == 0:
         logging.info('No container exists which corresponds to {}'.format(app_or_service_name))
