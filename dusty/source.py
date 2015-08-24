f816c754da4b1cc2c058ddeb62e1f8d38694fffd
Travis Thieman <travis.thieman@gmail.com>
2015-08-24 15:04:57 -0400

    Explain why we need a function for async repo update

--- dusty/source.py
+++ dusty/source.py
@@ -174,5 +174,8 @@ class Repo(object):
                               'please fix this and run `dusty up`'.format(self.managed_path))
 
     def update_local_repo_async(self, task_queue, force=False):
+        """Local repo updating suitable for asynchronous, parallel execution.
+        We still need to run `ensure_local_repo` synchronously because it
+        does a bunch of non-threadsafe filesystem operations."""
         self.ensure_local_repo()
         task_queue.enqueue_task(self.update_local_repo, force=force)
