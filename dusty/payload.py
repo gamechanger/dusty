c14873c4a551257e5835ef686f6a102fb0229128
Jordan Singleton <jordanwcsingleton@gmail.com>
2015-09-10 14:03:03 -0400

    docstring crazy yml constructor fn

--- dusty/payload.py
+++ dusty/payload.py
@@ -50,6 +50,10 @@ def get_payload_function(fn_key):
     return _daemon_command_mapping[fn_key]
 
 def init_yaml_constructor():
+    """
+    This dark magic is used to make yaml.safe_load encode all strings as utf-8,
+    where otherwise python unicode strings would be returned for non-ascii chars
+    """
     def utf_encoding_string_constructor(loader, node):
         return loader.construct_scalar(node).encode('utf-8')
     yaml.SafeLoader.add_constructor(u'tag:yaml.org,2002:str', utf_encoding_string_constructor)
