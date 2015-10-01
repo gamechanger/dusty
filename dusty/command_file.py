9311f966a72d0d7c252b0a9cd3af57ab29936df3
Travis Thieman <travis.thieman@gmail.com>
2015-10-01 15:50:20 -0400

    Raise if it makes no sense to have gotten here

--- dusty/command_file.py
+++ dusty/command_file.py
@@ -190,7 +190,7 @@ def make_test_command_files(app_or_lib_name, expanded_specs):
     app_or_lib_spec = expanded_specs.get_app_or_lib(app_or_lib_name)
     test_spec = app_or_lib_spec['test']
     if not test_spec:
-        return
+        raise RuntimeError('No tests defined for {}'.format(app_or_lib_name))
     _write_test_command(app_or_lib_spec, expanded_specs)
     for suite_spec in test_spec['suites']:
         _write_test_suite_command(app_or_lib_spec, suite_spec)
