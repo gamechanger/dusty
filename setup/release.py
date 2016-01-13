c2ae5af9860c4a2cdc07011ee0f002b887ea732d
Travis Thieman <travis.thieman@gmail.com>
2016-01-13 12:00:41 -0500

    Neat, this is also a context manager

--- setup/release.py
+++ setup/release.py
@@ -25,10 +25,9 @@ for binary in ['dusty']:
                              name=binary,
                              asset=f)
 
-tarball = tarfile.open('dusty.tar.gz', 'w:gz')
-tarball.add('dist/dusty', arcname='dusty')
-tarball.add('setup/com.gamechanger.dusty.plist', arcname='com.gamechanger.dusty.plist')
-tarball.close()
+with tarfile.open('dusty.tar.gz', 'w:gz') as tarball:
+    tarball.add('dist/dusty', arcname='dusty')
+    tarball.add('setup/com.gamechanger.dusty.plist', arcname='com.gamechanger.dusty.plist')
 
 with open('dusty.tar.gz', 'r') as f:
     release.upload_asset(content_type='application/octet-stream',
