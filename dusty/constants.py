9e6247eb4ef0f09d5cbac2df99450a4f00a5455d
Travis Thieman <travis.thieman@gmail.com>
2016-04-14 12:50:30 -0400

    Bump version constant to 0.7.2

--- dusty/constants.py
+++ dusty/constants.py
@@ -4,7 +4,7 @@ import re
 
 from pkg_resources import resource_string
 
-VERSION = '0.7.1'
+VERSION = '0.7.2'
 BINARY = False # overridden by PyInstaller when we build a binary
 PRERELEASE = False # overridden by PyInstaller when we build a prerelease binary
 
