272505b9681a13b33411089218117f53efeb12d1
Hesham Salman <hesham8@gmail.com>
2019-01-10 14:19:36 -0500

    Added a comma

--- dusty/systems/virtualbox/__init__.py
+++ dusty/systems/virtualbox/__init__.py
@@ -96,7 +96,7 @@ def _init_docker_vm():
         log_to_client('Initializing new Dusty VM with Docker Machine')
         machine_options = ['--driver', 'virtualbox',
                            '--virtualbox-cpu-count', '-1',
-                           '--virtualbox-boot2docker-url' constants.CONFIG_BOOT2DOCKER_URL,
+                           '--virtualbox-boot2docker-url', constants.CONFIG_BOOT2DOCKER_URL,
                            '--virtualbox-memory', str(get_config_value(constants.CONFIG_VM_MEM_SIZE)),
                            '--virtualbox-hostonly-nictype', constants.VM_NIC_TYPE]
         check_call_demoted(['docker-machine', 'create'] + machine_options + [constants.VM_MACHINE_NAME],
