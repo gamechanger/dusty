259fa40ebac2767e734ce59ea27264021e851b4c
Alex Young <alexyoung1992@gmail.com>
2019-01-14 13:51:26 -0500

    fix(create_binaries): pin python version

--- setup/create_binaries.sh
+++ setup/create_binaries.sh
@@ -2,7 +2,11 @@
 set -e
 venv_name=venv
 rm -rf $venv_name
-virtualenv $venv_name
+if [[ $(/usr/bin/python --version 2>&1) =~ .*2\.7.* ]]; then # --version outputs to stderr
+    virtualenv --python=/usr/bin/python $venv_name
+else
+    virtualenv $venv_name
+fi
 $venv_name/bin/pip install pyinstaller==3.4
 $venv_name/bin/pip install .
 $venv_name/bin/pyinstaller -F --runtime-hook=setup/binary-hook.py setup/dusty.spec
